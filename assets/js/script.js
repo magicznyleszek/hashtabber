49d3b94510e3492c1203be695cca3fdd6be5b17c
smutnyleszek <smutnyleszek@gmail.com>
2014-03-27 15:55:25 +0100

    configured some basic styles, added base script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,51 @@
 function projectInterface() {
-    this.init = init;
-    function init() {
-        // initialize your functions here
-        doSomething();
+    // declare a public run function
+    this.run = run;
+    function run() {
+        // initialize all functions here
+        hashTabber();
     }
-    // create your functions here
-    function doSomething() {
-        if(true === true) {
-            console.log("truth");
+
+    // --------------------------------------------------
+    // hashTabber
+    // --------------------------------------------------
+    function hashTabber() {
+        // declarations
+        var options = {
+            container: '.hashTabber',
+            navigation: '.hashTabber-navigation',
+            data: '.hashTabber-data',
+            activeClass: 'active'
+        };
+
+        // for activating target element and it's data element
+        function activateItem(_target) {
+            // get item parent node
+            var navList = $(_target.delegateTarget.parentNode);
+            // get clicked item index
+            var itemIndex = navList.find('>li').index(_target.delegateTarget);
+            // get the proper data element for the switcher
+            var dataList = navList.parent().find(options.data);
+            // clear the active classes for all elements
+            dataList.find('>li').removeClass(options.activeClass);
+            navList.find('>li').removeClass(options.activeClass);
+            // add active class to according li element from switcher
+            $(dataList.find('>li')[itemIndex]).addClass(options.activeClass);
+            $(navList.find('>li')[itemIndex]).addClass(options.activeClass);
         }
+        // check if content switcher exists
+        if ($(options.container).length > 0) {
+            // trigger on click
+            $(options.navigation).find('>li').click(activateItem);
+            // activate first element on start
+            $(options.navigation).find('>li')[0].click();
+        }
+
+
+
     }
 }
-var knowYourDinosaurs = new projectInterface();
-knowYourDinosaurs.init();
+// create a public object
+var hashTabberSoundsLikeDrugs = new projectInterface();
+// run functions
+hashTabberSoundsLikeDrugs.run();
