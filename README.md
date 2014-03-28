dc537ca311a7e88be9ba75fd9c7f337d319f6c09
smutnyleszek <smutnyleszek@gmail.com>
2014-03-28 17:24:46 +0100

    added default tab functionality, started making simple theme, updated boilerplate files

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-hashtabber
-==========
+# HashTabber
+
+This is a simple hash-driven tabbed navigation system. I was constantly in need of those, so I finally made a kinda-plugin. It uses some jQuery and SCSS.
+
+You can read more at [hashtabber.smutnyleszek.com](http://hashtabber.smutnyleszek.com).
+
+
+## Contact
+
+You can contact me at [smutnyleszek@gmail.com](mailto:smutnyleszek@gmail.com).
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,19 @@
 function projectInterface() {
+    // --------------------------------------------------
     // declare a public run function
+    // with all private stuff initialized inside of it
+    // --------------------------------------------------
+
     this.run = run;
     function run() {
-        // initialize all functions here
         hashTabber();
     }
+
 
     // --------------------------------------------------
     // hashTabber
     // --------------------------------------------------
+
     function hashTabber() {
         // declarations
         var tabber = {
@@ -17,6 +22,7 @@ function projectInterface() {
                 nav: '.hashTabber-nav',
                 data: '.hashTabber-data',
                 activeClass: 'active',
+                tab: 'data-defaultTab',
             },
             helpers: {
                 hashProber: function () {
@@ -33,12 +39,27 @@ function projectInterface() {
                 idsGiver: function () {
                     // loop through every instance of hashTabber
                     $(tabber.options.container).each(function () {
-                        // get the distinct id of current tabber
+                        // get the distinct id of current tabber and default tab
                         var distinctName = $(this).attr('id');
+                        var defaultTab = 0;
+                        // check if defaultTab html data is set
+                        if ($(this).attr(tabber.options.tab)) {
+                            defaultTab = parseInt($(this).attr(tabber.options.tab),10);
+                        }
                         // loop through every nav element of current tabber
                         $(this).find(tabber.options.nav).find('>li').each(function(index, item) {
                             // add #link to every one of these
                             $(item).find('>a').attr('href', '#' + distinctName + '=' + index);
+                            // set default to active
+                            if (index === defaultTab) {
+                                $(item).addClass(tabber.options.activeClass);
+                            }
+                        });
+                        $(this).find(tabber.options.data).find('>li').each(function(index, item) {
+                            // set default to active
+                            if (index === defaultTab) {
+                                $(item).addClass(tabber.options.activeClass);
+                            }
                         });
                     });
                     return true;
@@ -53,17 +74,14 @@ function projectInterface() {
                     } else {
                         number = 0;
                     }
-
                     // get the parent nav and data lists
                     var parentNav = grandpa.find(tabber.options.nav);
                     var parentData = grandpa.find(tabber.options.data);
-
                     // clear active class of all nav elements and give it to the target one
                     $(parentNav.find('>li')).removeClass(tabber.options.activeClass);
                     $(parentNav.find('>li')[number]).addClass(tabber.options.activeClass);
                     
                     // clear active class of all data elements and give it to the target one
-
                     $(parentData.find('>li')).removeClass(tabber.options.activeClass);
                     $(parentData.find('>li')[number]).addClass(tabber.options.activeClass);
 
@@ -77,39 +95,18 @@ function projectInterface() {
         // run for the first time on page load
         tabber.helpers.tabSwiper(tabber.helpers.hashProber());
 
-
         $(window).bind('hashchange', function () {
             tabber.helpers.tabSwiper(tabber.helpers.hashProber());
         });
-
-        // // for activating target element and it's data element
-        // function activateItem(_target) {
-        //     // get item parent node
-        //     var navList = $(_target.delegateTarget.parentNode);
-        //     // get clicked item index
-        //     var itemIndex = navList.find('>li').index(_target.delegateTarget);
-        //     // get the proper data element for the switcher
-        //     var dataList = navList.parent().find(options.data);
-        //     // clear the active classes for all elements
-        //     dataList.find('>li').removeClass(options.activeClass);
-        //     navList.find('>li').removeClass(options.activeClass);
-        //     // add active class to according li element from switcher
-        //     $(dataList.find('>li')[itemIndex]).addClass(options.activeClass);
-        //     $(navList.find('>li')[itemIndex]).addClass(options.activeClass);
-        // }
-        // // check if content switcher exists
-        // if ($(options.container).length > 0) {
-        //     // trigger on click
-        //     $(options.navigation).find('>li').click(activateItem);
-        //     // activate first element on start
-        //     $(options.navigation).find('>li')[0].click();
-        // }
-
-
-
     }
+
 }
-// create a public object
+
+
+// --------------------------------------------------
+// create a public object and start its run function
+// --------------------------------------------------
+
 var hashTabberSoundsLikeDrugs = new projectInterface();
 // run functions
 hashTabberSoundsLikeDrugs.run();
